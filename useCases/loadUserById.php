62f09e0219a4b0c22ea8888758e85de30d2f5ff5
damiangraber <damian.graver@gmail.com>
2016-08-28 12:46:33 +0000

    Twitter part 2

---
+++ useCases/loadUserById.php
@@ -0,0 +1,14 @@
+<?php 
+
+require_once '../src/User.php';
+require_once '../connection.php';
+
+$User = User::loadUserById($conn, 1);
+var_dump($User);
+
+
+$user2 = User::loadUserById($conn, 342);
+var_dump($user2);
+
+
+?>
